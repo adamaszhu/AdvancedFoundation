07bfb9a8fa471073dbc75b92aa492af92e2c4b9a
Adamas Zhu <adamas.zhu@outware.com.au>
2017-05-04 13:33:44 +1000

    Add LocationHelper

---
+++ AdvancedFoundationTests/Location/LocationHelperSpecs.swift
@@ -0,0 +1,11 @@
+class LocationHelperSpecs: QuickSpec {
+    
+    override func spec() {
+        // COMMENT: LocationHelper is highly related to hardware, so it should be tested manually.
+    }
+    
+}
+
+import Quick
+import Nimble
+@testable import AdvancedFoundation
