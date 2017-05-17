8e723d9d54e433c69a0ef47e7128cdacddc95d0f
Adamas Zhu <adamas.zhu@outware.com.au>
2017-05-17 14:17:29 +1000

    Finalise parser classes

--- AdvancedFoundationTests/Parser/JSONPath+Parser.swift
+++ AdvancedFoundationTests/Parser/JSONPath+Parser.swift
@@ -1,15 +1,15 @@
 class JSONPathParserSpecs: QuickSpec {
     
     override func spec() {
-        describe("calls parseString(_)") {
+        describe("calls parse(_)") {
             context("with correct format") {
-                let jsonPath = JSONPath.parseString("node1/node2[3]/node3")
+                let jsonPath = JSONPath.parse("node1/node2[3]/node3")
                 it("returns JSONPath with 3 nodes") {
                     expect(jsonPath?.nodes.count) == 3
                 }
             }
             context("with incorrect format") {
-                let jsonPath = JSONPath.parseString("node1/node23]/node3")
+                let jsonPath = JSONPath.parse("node1/node23]/node3")
                 it("returns nil") {
                     expect(jsonPath).to(beNil())
                 }
