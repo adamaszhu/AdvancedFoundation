575c5e3d3ac64fa1bbd7cb6a8ad0a6d4984f8328
Adamas Zhu <adamas.zhu@hotmail.com>
2017-06-23 20:15:48 +1000

    Fix the indent

--- AdvancedFoundation/Array/Array+Accessor.swift
+++ AdvancedFoundation/Array/Array+Accessor.swift
@@ -5,27 +5,27 @@
  * - date: 23/06/2017
  */
 public extension Array {
-  
-  /**
-   * System error.
-   */
-  private static var indexError: String  {
-    return "The index is out of rage."
-  }
-  
-  /**
-   * Get the element at an index with index checking.
-   * - parameter index: The index.
-   * - returns: The element. Nil if the index is invalid.
-   */
-  public func element(atIndex index: Int) -> Element? {
-    guard index >= 0, index < count else {
-      Logger.standard.logError(Array.indexError, withDetail: index)
-      return nil
+    
+    /**
+     * System error.
+     */
+    private static var indexError: String  {
+        return "The index is out of rage."
     }
-    return self[index]
-  }
-  
+    
+    /**
+     * Get the element at an index with index checking.
+     * - parameter index: The index.
+     * - returns: The element. Nil if the index is invalid.
+     */
+    public func element(atIndex index: Int) -> Element? {
+        guard index >= 0, index < count else {
+            Logger.standard.logError(Array.indexError, withDetail: index)
+            return nil
+        }
+        return self[index]
+    }
+    
 }
 
 import Foundation
