1521a9f5ca8790f069861a92b33828537d2873bf
Adamas Zhu <adamas.zhu@hotmail.com>
2017-07-12 07:38:56 +1000

    Improve the comment style of the logger

--- AdvancedFoundation/Log/Logger.swift
+++ AdvancedFoundation/Log/Logger.swift
@@ -1,69 +1,60 @@
-/**
- * Logger is used to format a log.
- * - author: Adamas
- * - version: 1.0.1
- * - date: 11/04/2017
- */
-public class Logger {
+/// Logger is used to format a log.
+/// - author: Adamas
+/// - version: 1.1.0
+/// - date: 12/07/2017
+final public class Logger {
     
-    /**
-     * The default logger.
-     */
+    /// The default logger.
     public static let standard: Logger = Logger()
     
-    /**
-     * Message tag.
-     */
+    /// Message tags.
     private static let errorTag = "Error"
     private static let infoTag = "Info"
     private static let warningTag = "Warning"
     private static let detailSeperator = "=========="
     
-    /**
-     * The date format.
-     */
+    /// The date format.
     private static let dateFormat = "yyyy/MM/dd HH:mm:ss.SSS"
     
-    /**
-     * Log an info.
-     * - parameter info: The info.
-     * - parameter detail: The detail of the info.
-     */
+    /// Log an info.
+    ///
+    /// - Parameters:
+    ///   - info: The info.
+    ///   - detail: The detail of the info.
     public func logInfo(_ info: String, withDetail detail: Any? = nil) {
         logMessage(info, withTag: Logger.infoTag, withDetail: detail)
     }
     
-    /**
-     * Log a warning.
-     * - parameter warning: The warning.
-     * - parameter detail: The detail of the warning.
-     */
+    /// Log a warning.
+    ///
+    /// - Parameters:
+    ///   - warning: The warning.
+    ///   - detail: The detail of the warning.
     public func logWarning(_ warning: String, withDetail detail: Any? = nil) {
         logMessage(warning, withTag: Logger.warningTag, withDetail: detail)
     }
     
-    /**
-     * Log an error.
-     * - parameter error: The error.
-     * - parameter detail: The detail of the error.
-     */
+    /// Log an error.
+    ///
+    /// - Parameters:
+    ///   - error: The error.
+    ///   - detail: The detail of the error.
     public func logError(_ error: String, withDetail detail: Any? = nil) {
         logMessage(error, withTag: Logger.errorTag, withDetail: detail)
     }
     
-    /**
-     * Log an error.
-     * - parameter error: The error.
-     */
+    /// Log an error.
+    ///
+    /// - Parameter error: The error.
     public func logError(_ error: Error) {
         logMessage(error.localizedDescription, withTag: Logger.errorTag)
     }
     
-    /**
-     * Log a message.
-     * - parameter tag: The type of the message.
-     * - parameter message: The message.
-     */
+    /// Log a message.
+    ///
+    /// - Parameters
+    ///   - tag: The type of the message.
+    ///   - message: The message.
     private func logMessage(_ message: String, withTag tag: String, withDetail detail: Any? = nil) {
         let date = Date()
         let dateFormatter = DateFormatter()
