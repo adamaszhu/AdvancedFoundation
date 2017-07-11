77b514555c1ae9ea26376c59c45b93dff401a4e7
Adamas Zhu <adamas.zhu@hotmail.com>
2017-07-11 23:02:58 +1000

    Format the array bundle

--- AdvancedFoundation/Array/Array+Accessor.swift
+++ AdvancedFoundation/Array/Array+Accessor.swift
@@ -1,23 +1,17 @@
-/**
- * Array+Accessor provides additional support to access the array.
- * - author: Adamas
- * - version: 1.1.0
- * - date: 23/06/2017
- */
+/// Array+Accessor provides additional support to access the array.
+///
+/// - author: Adamas
+/// - version: 1.1.0
+/// - date: 23/06/2017
 public extension Array {
     
-    /**
-     * System error.
-     */
-    private static var indexError: String  {
-        return "The index is out of rage."
-    }
+    /// System error.
+    private static var indexError: String { return "The index is out of rage." }
     
-    /**
-     * Get the element at an index with index checking.
-     * - parameter index: The index.
-     * - returns: The element. Nil if the index is invalid.
-     */
+    /// Get the element at an index with index checking.
+    ///
+    /// - Parameter index: The index.
+    /// - Returns: The element. Nil if the index is invalid.
     public func element(atIndex index: Int) -> Element? {
         guard index >= 0, index < count else {
             Logger.standard.logError(Array.indexError, withDetail: index)
