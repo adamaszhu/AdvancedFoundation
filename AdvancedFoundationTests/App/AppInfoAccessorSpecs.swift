f19444bf6aa3a927651979aef809f4d43a759911
Adamas Zhu <adamas.zhu@outware.com.au>
2017-09-13 17:49:34 +1000

    Update the version number

--- AdvancedFoundationTests/App/AppInfoAccessorSpecs.swift
+++ AdvancedFoundationTests/App/AppInfoAccessorSpecs.swift
@@ -23,7 +23,7 @@ class AppInfoAccessorSpecs: QuickSpec {
         describe("has version") {
             context("with valid bundle") {
                 it("is correct version") {
-                    expect(appInfoAccessor.version) == "1.1.3"
+                    expect(appInfoAccessor.version) == "1.1.4"
                 }
             }
             context("with invalid bundle") {
