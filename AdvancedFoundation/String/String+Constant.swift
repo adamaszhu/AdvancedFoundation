3e231a3d9476fa9e986ce05441928cd91fcf31ea
Adamas Zhu <account@adamaszhu.com>
2017-09-23 13:23:54 +1000

    Add the string constant file

---
+++ AdvancedFoundation/String/String+Constant.swift
@@ -0,0 +1,12 @@
+/// String+Constant defines some common string constant.
+///
+/// - author: Adamas
+/// - version: 1.1.4
+/// - date: 22/09/2017
+public extension String {
+    
+    // Constants
+    public static let space = " "
+    public static let linebreak = "\n"
+    public static let empty = ""
+}
