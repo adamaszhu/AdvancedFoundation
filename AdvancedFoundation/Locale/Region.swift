203b8718b19c4cfe7771723bb3b1a86d062cf881
Adamas Zhu <adamas.zhu@coles.com.au>
2023-08-12 11:02:34 +1000

    Make two enums be case interable

--- AdvancedFoundation/Locale/Region.swift
+++ AdvancedFoundation/Locale/Region.swift
@@ -4,7 +4,7 @@
 /// - version: 1.10.0
 /// - date: 11/08/2023
 /// - Warning: Since iOS 16, a `Region` struct is introduced in `Locale` 
-public enum Region: String {
+public enum Region: String, CaseIterable {
     case australia = "AU"
     case america = "US"
     case china = "CN"
