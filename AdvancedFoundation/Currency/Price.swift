410ad256d315887dc0f94682556a71c87116300b
Adamas Zhu <adamas.zhu@coles.com.au>
2023-09-02 10:16:11 +1000

    Isolate currency and add price

---
+++ AdvancedFoundation/Currency/Price.swift
@@ -0,0 +1,22 @@
+/// Price
+///
+/// - author: Adamas
+/// - version: 1.9.10
+/// - date: 02/09/2023
+public struct Price {
+
+    /// The currency of the price
+    let currency: Currency
+
+    /// The amount
+    let amount: Double
+
+    /// Initialize the price
+    /// - Parameters:
+    ///   - currency: The currency of the price
+    ///   - price: The amount
+    public init(currency: Currency, amount: Double) {
+        self.currency = currency
+        self.amount = amount
+    }
+}
